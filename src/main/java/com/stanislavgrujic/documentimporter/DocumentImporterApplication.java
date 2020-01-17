ca7090464249f7c482e0122437e800047f40656f
stanislavgrujic <sgrujic@execom.eu>
2020-01-17 13:39:05 +0100

    Initial commit

---
+++ src/main/java/com/stanislavgrujic/documentimporter/DocumentImporterApplication.java
@@ -0,0 +1,13 @@
+package com.stanislavgrujic.documentimporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentImporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stanislavgrujic/documentimporter/DocumentImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stanislavgrujic.documentimporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentImporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
