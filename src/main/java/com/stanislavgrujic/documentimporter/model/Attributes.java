cc366491f1213e08110c755a9c6db9f69494c7a2
stanislavgrujic <sgrujic@execom.eu>
2020-02-02 08:15:52 +0100

    Add new attributes

--- src/main/java/com/stanislavgrujic/documentimporter/model/Attributes.java
+++ src/main/java/com/stanislavgrujic/documentimporter/model/Attributes.java
@@ -17,8 +17,19 @@ public class Attributes {
    @SerializedName("part-of")
    private String partOf;
 
+   @SerializedName("source-link")
    private String sourceLink;
 
+   @SerializedName("source-author")
+   private String sourceAuthor;
 
+   @SerializedName("source-details")
+   private String sourceDetails;
+
+   @SerializedName("time-period")
+   private String timePeriod;
+
+   @SerializedName("tags")
+   private String tags;
 
 }
