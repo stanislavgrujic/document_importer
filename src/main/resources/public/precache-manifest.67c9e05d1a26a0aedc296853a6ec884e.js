cb803a850c1e1b0aa5bc095768cebb33699dec57
stanislavgrujic <sgrujic@execom.eu>
2020-02-20 21:24:36 +0100

    Add built frontend application

---
+++ src/main/resources/public/precache-manifest.67c9e05d1a26a0aedc296853a6ec884e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6082141e039941781775f39c4104833",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25248c85194273cd2f80",
+    "url": "/static/js/2.740431d6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.740431d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8eda5d68fe7a9046e9c",
+    "url": "/static/js/main.2dd07035.chunk.js"
+  },
+  {
+    "revision": "6cdd945800e2fc9c333a",
+    "url": "/static/js/runtime-main.c0d4f4ee.js"
+  }
+]);
